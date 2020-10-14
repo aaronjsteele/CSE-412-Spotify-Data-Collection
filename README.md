ba54f8c00fa9e64e4abdc46a49ec3880746e77f2
CactusClimber <ajsteele@asu.edu>
2020-10-14 15:43:32 -0700

    Added file to add random users

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ If you'd like to contribute, create a `config.py` file in your local repository 
  - `database` -- the name of the database
  
 Note that most of these values are private and will not be shared outside the group for this project.
+
+## Included Files
+The only two files of any importance in this repository at the moment are `api_get.py` and `generate_fake_data.py`. `api_get.py` pulls data from Spotify's servers to fill the database with some real data about songs, artists, albums, etc. Note that it isn't perfect right now, as it grabs the top songs for each artist then gets the album for each song. In the future it will be rewritten to grab albums for each artist _then_ the songs in each album.
+
+`generate_fake_data.py` generates a number of fake users and ratings. In particular, it creates fake users, fake 'listened_to' and 'rates' relationships (with gibberish comments) and also creates a number of fake relationships between artists and other artists, as well as artists 'appearing on' other albums. Some of this data may be made more realistic before the end of the project.
---
+++ generate_fake_data.py
@@ -0,0 +1,170 @@
+# --------------------------------------------------------
+# This file generates a number of fake usernames and
+# comments to place into the SQL server, as we do not
+# have real users. Some SQL queries are used to get the
+# necessary parameters (so we can pair up a fake user
+# with some song in the database, for example).
+# --------------------------------------------------------
+
+import psycopg2
+import random
+import string
+from faker import Faker
+import config
+
+# Thanks to https://pynative.com/python-generate-random-string/
+# for this code for a random string
+def get_random_alphanumeric_string(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
+    return result_str
+
+# --------------------------------------------------------
+# Connect to Database
+connection = psycopg2.connect(
+    host = config.host,
+    port = config.port,
+    user = config.user,
+    password = config.password,
+    database = config.database
+    )
+cursor=connection.cursor()
+# --------------------------------------------------------
+# First, we want to select all song_id values, so we can
+# choose some.
+cursor.execute("SELECT song_id FROM song")
+rows = cursor.fetchall()
+song_id_list = []
+for row in rows:
+    song_id_list.append(row[0].strip())
+
+# For artist relationships, we create all tuples that
+# are not artists related to themselves. We will then
+# add a random selection of these.
+cursor.execute("""
+    SELECT a1.artist_id, a2.artist_id
+    FROM artist AS a1, artist AS a2
+    WHERE a1.artist_id != a2.artist_id;
+""")
+rows = cursor.fetchall()
+artist_id_list = []
+for row in rows:
+    artist_id_list.append([row[0].strip(), row[1].strip()])
+
+# Finally, we select all tuples of artist_id and album_id
+# That DO NOT already have a relationship in
+# participates_in, so we can add one.
+# NOTE: In Spotify valid album_group values are "album",
+#       "single", "compilation", "appears_on". We will
+#       add "appears_on" (wrongly) to some of these
+cursor.execute("""
+    (SELECT artist_id, album_id FROM artist, album)
+    EXCEPT
+    (SELECT artist_id, album_id FROM participates_in)
+""")
+rows = cursor.fetchall()
+participates_in_list = []
+for row in rows:
+    participates_in_list.append([row[0].strip(), row[1].strip()])
+# --------------------------------------------------------
+# Now, let's generate some fake users to enter into our
+# database.
+fake = Faker()
+Faker.seed(1337)
+random.seed(10000)
+
+user_list = []
+for _ in range(100):
+    user_id = get_random_alphanumeric_string(21)
+    username = fake.simple_profile()['username']
+    user_list.append([user_id, username])
+
+# We have some users. Now they need to listen to
+# some songs. Each user listens to a random number
+# of songs from 10 - (# songs - 10) from the query
+# earlier.
+listens_to_list = []
+for el in user_list:
+    songs_listened_to = random.sample(song_id_list, random.randint(10, len(song_id_list) - 10))
+    for song_id in songs_listened_to:
+        listens_to_list.append([el[0], song_id])
+
+# A similar approach is given to construct the
+# data for the 'rates' table, except additional
+# data is added for the rating_value (an integer
+# from 1 to 10) and a comment
+rates_list = []
+for el in user_list:
+    songs_rated = random.sample(song_id_list, random.randint(10, 30))
+    for song_id in songs_rated:
+        rates_list.append([el[0], song_id, random.randint(1,10), fake.sentence()])
+
+# Selects a random amount of artists to be related
+# to each other.
+related_to_list = random.sample(artist_id_list, random.randint(30, 50))
+
+# Finally, we add some random elements to the
+# participates_in relationship with an album_group
+# of "appears_on"
+participates_in_to_add = []
+temp = random.sample(participates_in_list, 100)
+for row in temp:
+    participates_in_to_add.append(["appears_on" ,row[0], row[1]])
+
+# --------------------------------------------------------
+# Finally, we add our data to the Database
+
+print("Now adding elements to \'user_table\'")
+for el in user_list:
+    print("adding ", el)
+    cursor.execute("""
+        INSERT INTO user_table (user_id, display_name)
+        VALUES (%s, %s) ON CONFLICT DO NOTHING;
+        """,
+        (el[0], el[1])
+    )
+
+print("Now adding elements to \'listens_to\'")
+for el in listens_to_list:
+    print("adding ", el)
+    cursor.execute("""
+        INSERT INTO listens_to (user_id, song_id)
+        VALUES (%s, %s) ON CONFLICT DO NOTHING;
+        """,
+        (el[0], el[1])
+    )
+
+print("Now adding elements to \'rates\'")
+for el in rates_list:
+    print("adding ", el)
+    cursor.execute("""
+        INSERT INTO rates (user_id, song_id, rating_value, comment)
+        VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING;
+        """,
+        (el[0], el[1], el[2], el[3])
+    )
+
+print("Now adding elements to \'related_artists\'")
+for el in related_to_list:
+    print("adding ", el)
+    cursor.execute("""
+        INSERT INTO related_artists (artist_id_1, artist_id_2)
+        VALUES (%s, %s) ON CONFLICT DO NOTHING;
+        """,
+        (el[0], el[1])
+    )
+
+print("Now adding elements to \'participates_in\'")
+for el in participates_in_to_add:
+    print("adding ", el)
+    cursor.execute("""
+        INSERT INTO participates_in (album_group, artist_id, album_id)
+        VALUES (%s, %s, %s) ON CONFLICT DO NOTHING;
+        """,
+        (el[0], el[1], el[2])
+    )
+
+connection.commit()
+
+cursor.close()
+connection.close()
