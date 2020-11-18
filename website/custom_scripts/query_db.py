b86a813ac30ea695a4b52ff0f2c2808aacea3617
CactusClimber <ajsteele@asu.edu>
2020-11-18 16:03:37 -0700

    Fixed search bar, reorganized changes. New query_db file to place queries

---
+++ website/custom_scripts/query_db.py
@@ -0,0 +1,63 @@
+import psycopg2
+
+def test():
+    print("hello")
+    return
+
+# Initiates connection to DB server, returns a connection
+def init_db_connection():
+    import website.config as config
+    connection = psycopg2.connect(
+        host = config.host,
+        port = config.port,
+        user = config.user,
+        password = config.password,
+        database = config.database
+    )
+    return connection
+    """
+    cursor.execute(query)
+    results = cursor.fetchall()
+    keys = [column.name for column in cursor.description]
+    connection.close()
+    return [DotDict({key: data for key, data in zip(keys, row)}) for row in results]
+    """
+
+query = f"SELECT song_name as title, artist_name as artist FROM song, performed_by, artist "\
+                f"WHERE song.song_id = performed_by.song_id "\
+                f"AND performed_by.artist_id = artist.artist_id"
+
+def search_by_artist(cursor, input_str):
+    """
+    An example function to perform a query where we are searching by artist.
+    Note that the cursor and search term is passed in.
+
+    NOTE: Due to psycopg2 syntax, a '%' symbol in the LIKE clause
+    must be escaped with a '%'.
+    """
+    query = cursor.mogrify(
+        """
+        SELECT song_name AS title, artist_name AS artist
+        FROM song, performed_by, artist
+        WHERE song.song_id = performed_by.song_id
+            AND performed_by.artist_id = artist.artist_id
+            AND artist_name ILIKE %s
+        """,
+        (format_like_query(input_str),)
+    )
+    print(query)
+    cursor.execute(query)
+    results = cursor.fetchall()
+    keys = [column.name for column in cursor.description]
+    return [DotDict({key: data for key, data in zip(keys, row)}) for row in results]
+
+def format_like_query(input_str):
+    return '%' + input_str + '%'
+
+class DotDict(dict):
+	def __getattr__(self, key):
+		if key not in self:
+			print(f"There was an error while trying to access '{key}' from {self}")
+			return "Database Error"
+		else:
+			return self[key]
--- website/routes/__init__.py
+++ website/routes/__init__.py
@@ -1,22 +1,22 @@
 from website import app
-from flask import render_template, request
+from flask import render_template, request, g
 import os
+from website.custom_scripts import *
 
-
+# NOTE: g is a flask global variable for the current context.
 @app.route("/", methods=["GET", "POST"])
 def main():
     if request.method == "GET":
         return render_template("index.html", a=[1, 2, 3])
     elif request.method == "POST":
         # logic for querying database and return data
-        query = f"SELECT song_name as title, artist_name as artist FROM song, performed_by, artist "\
-                f"WHERE song.song_id = performed_by.song_id "\
-                f"AND performed_by.artist_id = artist.artist_id"
+        search_string = request.form['inputString']
+        query_type = request.form['inputQuery']
 
-        return render_template("index.html", songs=execute_query(query))
-        #return "{first_name}, fuck off".format(first_name=request.form.get("firstname", "???"))
+        return render_template("index.html", songs=query_db.search_by_artist(get_db().cursor(), search_string))
+        #return "{first_name}, bye".format(first_name=request.form.get("firstname", "???"))
     else:
-        return "fuck yourself", 405
+        return "oh no", 405
 
 @app.route("/songs-by-artist", methods=["GET"])
 def songs_by_artist():
@@ -29,6 +29,9 @@ def songs_by_artist():
 		print(f"/songs-by-artist needs to recieve an 'artist' parameter (eg. /songs-by-artist?artist=bob)")
 		return error_page()
 
+	# Queries are moved to the 'custom_scripts' (name pending)
+	# folder for organization, so we can chuck our queries or processing in another file
+
 	query = f"SELECT song_name as title, artist_name as artist FROM song, performed_by, artist "\
 			f"WHERE song.song_id = performed_by.song_id "\
 			f"AND performed_by.artist_id = artist.artist_id "\
@@ -39,6 +42,7 @@ def songs_by_artist():
 def error_page():
 	return "" # should be a template or something
 
+"""
 def execute_query(query):
 	import website.config as config
 	import psycopg2
@@ -55,6 +59,7 @@ def execute_query(query):
 	keys = [column.name for column in cursor.description]
 	connection.close()
 	return [DotDict({key: data for key, data in zip(keys, row)}) for row in results]
+"""
 
 class DotDict(dict):
 	def __getattr__(self, key):
@@ -63,3 +68,18 @@ class DotDict(dict):
 			return "Database Error"
 		else:
 			return self[key]
+
+def get_db():
+	"""
+	Opens a new connection to the DB if there is none for the current context.
+	"""
+	if not hasattr(g, 'postgres_db'):
+		g.postgres_db = query_db.init_db_connection()
+	return g.postgres_db
+
+
+@app.teardown_appcontext
+def close_db(input):
+	#Closes the database again at the end of the request.
+	if hasattr(g, 'postgres_db'):
+		g.postgres_db.close()
