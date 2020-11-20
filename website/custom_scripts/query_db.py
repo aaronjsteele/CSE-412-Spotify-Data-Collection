64618077d739eb77435fc4670cef2b918c5c8c36
CactusClimber <ajsteele@asu.edu>
2020-11-20 15:11:28 -0700

    Added additional query functionality, as well as average rating/number of listens

--- website/custom_scripts/query_db.py
+++ website/custom_scripts/query_db.py
@@ -15,57 +15,34 @@ def init_db_connection():
         database = config.database
     )
     return connection
-    """
-    cursor.execute(query)
-    results = cursor.fetchall()
-    keys = [column.name for column in cursor.description]
-    connection.close()
-    return [DotDict({key: data for key, data in zip(keys, row)}) for row in results]
-    """
-#is this doing anything here? Can we delete?
-query = f"SELECT song_name AS title, artist_name AS artist, album_name AS album FROM song, performed_by, artist "\
-        f"WHERE song.song_id = performed_by.song_id "\
-        f"AND performed_by.artist_id = artist.artist_id"
-'''
-def search_by_artist(cursor, input_str, query_type):
-    """
-    An example function to perform a query where we are searching by artist.
-    Note that the cursor and search term is passed in.
 
-    Queries get results by similar artist, album, or song title.
-
-    NOTE: Due to psycopg2 syntax, wrap strings in the format_like_query
-    function when calling an ILIKE query.
-    """
-    query = cursor.mogrify(
-        """
-        SELECT song_name AS title, artist_name AS artist, album_name AS album
-        FROM song, performed_by, artist, album, is_in
-        WHERE song.song_id = performed_by.song_id
-            AND performed_by.artist_id = artist.artist_id
-            AND song.song_id = is_in.song_id
-            AND is_in.album_id = album.album_id
-            AND artist_name ILIKE %s
-        """,
-        (format_like_query(input_str),)
-    )
-    query = cursor.mogrify(base_query, (format_like_query(input_str),))
-    print(query)
-    cursor.execute(query)
-    results = cursor.fetchall()
-    keys = [column.name for column in cursor.description]
-    return [DotDict({key: data for key, data in zip(keys, row)}) for row in results]
-'''
 def list_all(cursor, sort_by_type):
     # function that list all the songs when the search bar is empty
     sort_by_str = sort_by_parser(sort_by_type)
-    query = f"SELECT song_name AS title, artist_name AS artist "\
-            f"FROM song, artist, album, performed_by, is_in "\
-            f"WHERE song.song_id = performed_by.song_id "\
-            f"AND performed_by.artist_id = artist.artist_id "\
-            f"AND song.song_id = is_in.song_id "\
-            f"AND is_in.album_id = album.album_id "\
-            f"ORDER BY {sort_by_str}"
+    unprocessed_query = (
+        f"SELECT song_name AS title, artist_name AS artist, album_name AS album, avg_table.avg_rating AS average, count_table.total AS num_listens "\
+        f"FROM song, artist, performed_by, album, is_in, "\
+        f"    ( "\
+        f"        SELECT song.song_id, ROUND(AVG(rates.rating_value),2) AS avg_rating "\
+        f"        FROM song, rates "\
+        f"        WHERE song.song_id = rates.song_id "\
+        f"        GROUP BY song.song_id "\
+        f"    ) AS avg_table, "\
+        f"    ( "\
+        f"        SELECT song.song_id, COUNT(*) AS total "\
+        f"        FROM song, listens_to "\
+        f"        WHERE song.song_id = listens_to.song_id "\
+        f"        GROUP BY song.song_id "\
+        f"    ) AS count_table "\
+        f"WHERE song.song_id = performed_by.song_id  "\
+        f"    AND performed_by.artist_id = artist.artist_id  "\
+        f"    AND song.song_id = is_in.song_id  "\
+        f"    AND is_in.album_id = album.album_id  "\
+        f"    AND avg_table.song_id = song.song_id "\
+        f"    AND count_table.song_id = song.song_id "\
+        f"ORDER BY {sort_by_str}"
+    )
+    query = cursor.mogrify(unprocessed_query)
     cursor.execute(query)
     results = cursor.fetchall()
     keys = [column.name for column in cursor.description]
@@ -74,16 +51,49 @@ def list_all(cursor, sort_by_type):
 def search_by(cursor, input_str, query_type, sort_by_type): 
     # function that performs a query based on the query type
     sort_by_str = sort_by_parser(sort_by_type)
-    query = f"SELECT song_name AS title, artist_name AS artist "\
+    unprocessed_query = (
+        f"SELECT song_name AS title, artist_name AS artist, album_name AS album, avg_table.avg_rating AS average, count_table.total AS num_listens "\
+        f"FROM song, artist, performed_by, album, is_in, "\
+        f"    ( "\
+        f"        SELECT song.song_id, ROUND(AVG(rates.rating_value),2) AS avg_rating "\
+        f"        FROM song, rates "\
+        f"        WHERE song.song_id = rates.song_id "\
+        f"        GROUP BY song.song_id "\
+        f"    ) AS avg_table, "\
+        f"    ( "\
+        f"        SELECT song.song_id, COUNT(*) AS total "\
+        f"        FROM song, listens_to "\
+        f"        WHERE song.song_id = listens_to.song_id "\
+        f"        GROUP BY song.song_id "\
+        f"    ) AS count_table "\
+        f"WHERE song.song_id = performed_by.song_id  "\
+        f"    AND performed_by.artist_id = artist.artist_id  "\
+        f"    AND song.song_id = is_in.song_id  "\
+        f"    AND is_in.album_id = album.album_id  "\
+        f"    AND avg_table.song_id = song.song_id "\
+        f"    AND count_table.song_id = song.song_id "\
+        f"    AND {query_type}.{query_type}_name ILIKE %s "\
+        f"ORDER BY {sort_by_str}"
+    )
+    """
+    unprocessed_query = (
+            f"SELECT song_name AS title, artist_name AS artist, album_name AS album, AVG()"\
             f"FROM song, artist, performed_by, album, is_in "\
             f"WHERE song.song_id = performed_by.song_id "\
             f"AND performed_by.artist_id = artist.artist_id "\
             f"AND song.song_id = is_in.song_id "\
             f"AND is_in.album_id = album.album_id "\
-            f"AND {query_type}_name = '{input_str}' "\
-            f"ORDER BY {sort_by_str}"
+            f"AND rates.song_id = song.song_id "\
+            f"AND {query_type}_name ILIKE %s "\
+            f"ORDER BY {sort_by_str} "\
+            f"GROUP BY song.song_id"
+    )
+    """
+    query = cursor.mogrify(unprocessed_query, (format_like_query(input_str),))
+    #print(query)
     cursor.execute(query)
-    results = cursor.fetchall()
+    results = cursor.fetchmany(1000)
+    #print(results)
     keys = [column.name for column in cursor.description]
     return [DotDict({key: data for key, data in zip(keys, row)}) for row in results]
 
--- website/routes/__init__.py
+++ website/routes/__init__.py
@@ -7,64 +7,63 @@ from website.custom_scripts import *
 @app.route("/", methods=["GET", "POST"])
 def main():
     if request.method == "GET":
-        return render_template("index.html")
+        return render_template("index.html", prev=["", "", ""])
     elif request.method == "POST":
         # logic for querying database and return data
         search_string = request.form['inputString']
         query_type = request.form['inputQuery']
         sort_by_type = request.form['sortby']
+        prev_query = [search_string, query_type, sort_by_type]
         if search_string == "":
-            return render_template("index.html", songs=query_db.list_all(get_db().cursor(), sort_by_type))
+            return render_template("index.html", prev=["", "", ""], songs=query_db.list_all(get_db().cursor(), sort_by_type))
         else:
-            return render_template("index.html", songs=query_db.search_by(get_db().cursor(), search_string, query_type, sort_by_type))
-    else:
-        return "oh no", 405
+            return render_template("index.html", prev=prev_query, songs=query_db.search_by(get_db().cursor(), search_string, query_type, sort_by_type))
 
 @app.route("/songs-by-artist", methods=["GET"])
 def songs_by_artist():
-	if request.method != "GET":
-		print(f"/songs-by-artist recieved a {request.method} request when it should have recieved a 'GET' request.")
-		return error_page()
-	artist = request.args.get("artist", "")
+    if request.method != "GET":
+        print(f"/songs-by-artist received a {request.method} request when it should have recieved a 'GET' request.")
+        return error_page()
+    artist = request.args.get("artist", "")
 
-	if not artist:
-		print(f"/songs-by-artist needs to recieve an 'artist' parameter (eg. /songs-by-artist?artist=bob)")
-		return error_page()
+    if not artist:
+        print(f"/songs-by-artist needs to receive an 'artist' parameter (eg. /songs-by-artist?artist=bob)")
+        return error_page()
 
-	# Queries are moved to the 'custom_scripts' (name pending)
-	# folder for organization, so we can chuck our queries or processing in another file
+    # Queries are moved to the 'custom_scripts' (name pending)
+    # folder for organization, so we can chuck our queries or processing in another file
 
-	query = f"SELECT song_name as title, artist_name as artist FROM song, performed_by, artist "\
-			f"WHERE song.song_id = performed_by.song_id "\
-			f"AND performed_by.artist_id = artist.artist_id "\
-			f"AND artist_name='{artist}'"
+    query = f"SELECT song_name as title, artist_name as artist FROM song, performed_by, artist "\
+            f"WHERE song.song_id = performed_by.song_id "\
+            f"AND performed_by.artist_id = artist.artist_id "\
+            f"AND artist_name='{artist}'"
 
-	return render_template("index.html", songs=execute_query(query))
+    return render_template("index.html", songs=execute_query(query))
 
 def error_page():
-	return "" # should be a template or something
+    return "" # should be a template or something
 
 class DotDict(dict):
-	def __getattr__(self, key):
-		if key not in self:
-			print(f"There was an error while trying to access '{key}' from {self}")
-			return "Database Error"
-		else:
-			return self[key]
+    def __getattr__(self, key):
+        if key not in self:
+            print(f"There was an error while trying to access '{key}' from {self}")
+            return "Database Error"
+        else:
+            return self[key]
 
 def get_db():
-	"""
-	Opens a new connection to the DB if there is none for the current context.
-	"""
-	if not hasattr(g, 'postgres_db'):
-		g.postgres_db = query_db.init_db_connection()
-	return g.postgres_db
+    """
+    Opens a new connection to the DB if there is none for the current context.
+    """
+    if not hasattr(g, 'postgres_db'):
+        g.postgres_db = query_db.init_db_connection()
+    return g.postgres_db
 
 
 @app.teardown_appcontext
 def close_db(input):
-	"""
-	Closes the database again at the end of the request.
-	"""
-	if hasattr(g, 'postgres_db'):
-		g.postgres_db.close()
+    """
+    Closes the database again at the end of the request.
+    """
+    if hasattr(g, 'postgres_db'):
+        g.postgres_db.close()
