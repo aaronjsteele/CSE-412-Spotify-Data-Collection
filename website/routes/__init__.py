448779ced58ccf37127f6134dbf76ab6dadf1125
narnix17 <sabrinaxie11@gmail.com>
2020-11-21 16:15:39 -0700

    moved all queries to query_db.py

--- website/routes/__init__.py
+++ website/routes/__init__.py
@@ -1,11 +1,12 @@
 from website import app
-from flask import render_template, request, g
+from flask import render_template, request, g, redirect
 import os
+import sys
 from website.custom_scripts import *
 
 # NOTE: g is a flask global variable for the current context.
 @app.route("/", methods=["GET", "POST"])
-def main():
+def main_page():
     if request.method == "GET":
         return render_template("index.html", prev=["", "", ""])
     elif request.method == "POST":
@@ -20,80 +21,52 @@ def main():
             return render_template("index.html", prev=prev_query, songs=query_db.search_by(get_db().cursor(), search_string, query_type, sort_by_type))
 
 @app.route("/songs-by-artist", methods=["GET"])
-def songs_by_artist():
+def songs_by_artist_page():
     if request.method != "GET":
         print(f"/songs-by-artist received a {request.method} request when it should have recieved a 'GET' request.")
         return error_page()
     artist = request.args.get("artist", "")
-
     if not artist:
         print(f"/songs-by-artist needs to receive an 'artist' parameter (eg. /songs-by-artist?artist=bob)")
         return error_page()
-
-    # Queries are moved to the 'custom_scripts' (name pending)
-    # folder for organization, so we can chuck our queries or processing in another file
-
-    query = f"SELECT song_name as title, artist_name as artist FROM song, performed_by, artist "\
-            f"WHERE song.song_id = performed_by.song_id "\
-            f"AND performed_by.artist_id = artist.artist_id "\
-            f"AND artist_name='{artist}'"
-
-    return render_template("index.html", songs=execute_query(query))
-
-def error_page():
-    return "" # should be a template or something
+    return render_template("index.html", prev=["", " ", " "], songs=query_db.songs_by_artist(get_db().cursor(), artist))
 
 @app.route("/rate", methods=["GET"])
 def rate_song_page():
-	if request.method != "GET":
-		print(f"/rate recieved a {request.method} request when it should have recieved a 'GET' request.")
-		return error_page()
-	song = request.args.get("song", "")
-
-	if not song:
-		print(f"/rate needs to recieve a song-id parameter (eg. /rate?song=123)")
-		return error_page()
-
-	query = f"SELECT song_id as id, song_name as title "\
-			f"FROM song "\
-			f"WHERE song_id='{song}'"
-
-	results = execute_query(query)[0]
-
-	if not results:
-		print(f"/rate was unable to find any songs with id '{song}'")
-		return error_page()
-
-	return render_template("rate.html", song=results)
+    if request.method != "GET":
+        print(f"/rate recieved a {request.method} request when it should have recieved a 'GET' request.")
+        return error_page()
+    song = request.args.get("song", "")
+    if not song:
+        print(f"/rate needs to recieve a song-id parameter (eg. /rate?song=123)")
+        return error_page()
+    results = query_db.rate_song_page(get_db().cursor(), song)
+    if not results:
+        print(f"/rate was unable to find any songs with id '{song}'")
+        return error_page()
+    return render_template("rate.html", song=results)
 
 @app.route("/rate/<song_id>", methods=["POST"])
 def rate(song_id):
-	if request.method != "POST":
-		print(f"failed")
-		return error_page()
+    if request.method != "POST":
+        print(f"failed")
+        return error_page()
+    user_id = request.form['user_id']
+    rating_value = int(request.form['rating'])
+    comment = request.form['comment']
+    if not 1 <= rating_value <= 10:
+        print(f"/rate/ expected the rating to be between 1 and 10, but instead recieved {rating_value}")
+        return error_page()
+    query_db.rate_song_page(get_db().cursor(), user_id, song_id, rating_value, comment)		
+    return redirect("/")
 
-	user_id = request.form['user_id']
-	rating_value = int(request.form['rating'])
-	comment = request.form['comment']
-
-	if not 1 <= rating_value <= 10:
-		print(f"/rate/ expected the rating to be between 1 and 10, but instead recieved {rating_value}")
-		return error_page()
-
-	with Database() as cursor:
-		cursor.execute(
-			f"INSERT INTO rates (user_id, song_id, rating_value, comment) "\
-			f"VALUES ('{user_id}', '{song_id}', '{rating_value}', '{comment}') "\
-			f"ON CONFLICT DO NOTHING")
-
-	return ""
-#	return redirect("/")
+def error_page():
+    return "FUCK" # should be a template or something
 
 @app.errorhandler(404)
 def error404(error):
-	print(error)
-	return error_page()
-
+    print(error)
+    return error_page()
 
 class DotDict(dict):
     def __getattr__(self, key):
